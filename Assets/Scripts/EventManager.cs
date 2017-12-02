d136fc7fa7657af82a22f7de868e7fae748c4a67
Quixatocs <quixatocs@gmail.com>
2017-12-02 05:21:32 +0000

    Initial input and random rotation

---
+++ Assets/Scripts/EventManager.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EventManager : MonoBehaviour {
+
+    public static EventManager instance = null;
+
+
+    void Awake()
+    {
+        singleton();
+    }
+
+    void singleton()
+    {
+        if (instance == null)
+            instance = this;
+        else if (instance != this)
+            Destroy(gameObject);
+        DontDestroyOnLoad(gameObject);
+    }
+
+
+
+
+    public static void invokeSubscribersTo_Method()
+    {
+        //input code here
+    }
+
+
+    
+}
---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,76 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour {
+
+    int difficulty = 1;
+
+    public GameObject balancePole;
+    private float currentRotation = 0;
+    private float randomRotationAmount = 0;
+    private float playerRotationAmount = 0f;
+    private float pressureAmount = 0.1f;
+    private float decayAmount = 0.01f;
+
+
+    void Update () {
+
+        if (Input.GetMouseButton(0))
+        {
+            playerRotationAmount -= pressureAmount;
+        }
+
+        if (Input.GetMouseButton(1))
+        {
+            playerRotationAmount += pressureAmount;
+        }
+
+        RotateBalancePole();
+    }
+
+    void Start()
+    {
+        InvokeRepeating("ChangeRotationAmount", 1.0f, 1.0f);
+    }
+
+    private void RotateBalancePole()
+    {
+        AddRandomRotationToCurrent();
+        AddPlayerControlledRotationToCurrent();
+        balancePole.transform.rotation = Quaternion.Euler(new Vector3(0, 0, currentRotation));
+    }
+
+    private void AddRandomRotationToCurrent()
+    {
+        currentRotation += randomRotationAmount;
+    }
+
+    private void AddPlayerControlledRotationToCurrent()
+    {
+        if (Mathf.Abs(playerRotationAmount) <= 0.02f)
+        {
+            playerRotationAmount = 0f;
+        }
+
+        if (playerRotationAmount < 0.02)
+        {
+            playerRotationAmount += decayAmount;
+        }
+
+        if (playerRotationAmount > 0.02)
+        {
+            playerRotationAmount -= decayAmount;
+        }
+
+
+        currentRotation += playerRotationAmount;
+    }
+
+    private void ChangeRotationAmount()
+    {
+        randomRotationAmount = Random.Range(-0.5f, 0.5f);
+    }
+
+
+}
